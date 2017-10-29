c31969744fd3e90968bb6fba58b8fa2d3d69702f
xlorenzana <lorenzanae@wit.edu>
2017-10-29 00:08:27 -0400

    Changes to Station

--- project/src/edu/wit/dcsn/comp2000/queueapp/Main.java
+++ project/src/edu/wit/dcsn/comp2000/queueapp/Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(final String... args) throws IOException, IllegalAccessException {
+//    	System.out.println(new File("").getAbsolutePath());
         final ConfigImpl config = (ConfigImpl) ConfigHandler.loadConfig(new File("TrainSimulation.config"), new ConfigImpl());
         System.out.println(String.format("Ticks: %d", config.getTicks()));
         for(final String s: config.getInitialPassengersList()) {
