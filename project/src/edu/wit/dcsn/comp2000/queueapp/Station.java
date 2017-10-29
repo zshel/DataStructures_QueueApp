0d5fe29a33650f8615947b9e2fc80c53e46fff74
xlorenzana <lorenzanae@wit.edu>
2017-10-29 16:52:31 -0400

    Station changes 2

--- project/src/edu/wit/dcsn/comp2000/queueapp/Station.java
+++ project/src/edu/wit/dcsn/comp2000/queueapp/Station.java
@@ -1,15 +1,30 @@
 package edu.wit.dcsn.comp2000.queueapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.pearson.carrano.ArrayQueue;
 import com.pearson.carrano.QueueInterface;
 
 public class Station {
-
-    private QueueInterface<Passenger> passengerQueue = new ArrayQueue<>();
-
-    private int id;
+	
+	private QueueInterface<Passenger> IncomingQueue = new ArrayQueue<>();
+    
+    private QueueInterface<Passenger> OutgoingQueue = new ArrayQueue<>();
+    
+	
+    private int Stationid;
 
     public Station(final int id) {
-        this.id = id;
+        this.Stationid = id;
     }
+    
+    public void enterStation(Passenger passenger){
+    	IncomingQueue.enqueue(passenger);
+    }
+    
+    public void leaveStation(Train passenger){
+    	OutgoingQueue.dequeue();
+    }
+    
 }
